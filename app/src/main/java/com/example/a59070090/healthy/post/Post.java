44b4f1c6005ccdb6e4f1599f7b7d875e4d4572ea
Adobe Beta <adobebeta1997@gmail.com>
2018-11-19 20:27:12 +0700

    add retrofit Class Post

---
+++ app/src/main/java/com/example/a59070090/healthy/post/Post.java
@@ -0,0 +1,12 @@
+package com.example.a59070090.healthy.post;
+
+/**
+ * Created by adobebeta on 19/11/2018 AD.
+ */
+
+public class Post {
+    private String userId;
+    private String id;
+    private String title;
+    private String body;
+}
