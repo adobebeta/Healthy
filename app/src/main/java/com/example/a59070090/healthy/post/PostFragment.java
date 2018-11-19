97f237bcad2620d34677b84738801bd3233693a3
Adobe Beta <adobebeta1997@gmail.com>
2018-11-19 20:00:16 +0700

    add PostFragment pull data from okhttp
    dont pull to list T_T

--- app/src/main/java/com/example/a59070090/healthy/post/PostFragment.java
+++ app/src/main/java/com/example/a59070090/healthy/post/PostFragment.java
@@ -1,26 +1,40 @@
 package com.example.a59070090.healthy.post;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.a59070090.healthy.MenuFragment;
 import com.example.a59070090.healthy.R;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 /**
  * Created by adobebeta on 19/11/2018 AD.
  */
 
-public class PostFragment extends Fragment{
+public class PostFragment extends Fragment {
+
+    private OkHttpClient client;
+    private TextView result;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_post,container,false);
+        return inflater.inflate(R.layout.fragment_post, container, false);
 
     }
 
@@ -30,17 +44,61 @@ public class PostFragment extends Fragment{
 
         initBackBtnPressed();
 
+        result = getView().findViewById(R.id.result);
+        client = new OkHttpClient();
+
+        getWebservice();
+
+
+
     }
 
-    void initBackBtnPressed(){
-        Button backBtn =  getView().findViewById(R.id.back);
+    private void getWebservice() {
+        final Request request = new Request.Builder()
+                .url("https://jsonplaceholder.typicode.com/posts")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.setText("Failure");
+                    }
+                });
+
+            }
+
+            @Override
+            public void onResponse(final Response response) throws IOException {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            result.setText(response.body().string());
+                        }catch (IOException ioe){
+                            result.setText("Error during get body");
+                        }
+
+                    }
+                });
+
+            }
+        });
+    }
+
+
+    void initBackBtnPressed() {
+        Button backBtn = getView().findViewById(R.id.back);
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getActivity()
                         .getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.main_view,new MenuFragment())
+                        .replace(R.id.main_view, new MenuFragment())
                         .addToBackStack(null)
                         .commit();
             }
